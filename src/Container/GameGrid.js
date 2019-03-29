8427f7e44bf7591ccde481afa23e6ad85988f755
Sumit Saini <sumitsaini@UCLP1578.local>
2019-03-30 03:02:36 +0530

    code done

--- src/Container/GameGrid.js
+++ src/Container/GameGrid.js
@@ -3,49 +3,45 @@ import _ from 'lodash';
 import "./GameGrid.css";
 
 class GameGrid extends Component{
-    
+
     state = {
             game: [
                 [0,0,0,0],
                 [0,0,0,0],
                 [0,0,0,0],
                 [0,0,0,0]
-            ]
+            ],
+            game_status: "not_started"
         }
 
     componentDidMount() {
-        document.addEventListener("keydown", this.onKeyPressed.bind(this));
+        document.addEventListener("keyup", this.onKeyPressed.bind(this));
     }
     
     componentWillUnmount() {
-        document.removeEventListener("keydown", this.onKeyPressed.bind(this));
+        document.removeEventListener("keyup", this.onKeyPressed.bind(this));
     }      
 
     onKeyPressed = (e) => {
-        if( [ 37, 38, 39, 40].indexOf( e.keyCode ) !== -1){
-            switch(e.keyCode){
-                    case 37: this.handleLeftKeyPress();
-                        break;
-                    case 39: this.handleRightKeyPress();
-                        break;
-                    case 38: this.handleTopKeyPress();
-                        break;
-                    case 40: this.handleBottomKeyPress();
-                        break;
-                    default:
-                        break;
+        console.log(e);
+        if(this.state.game_status === "started"){
+            if( [ 37, 38, 39, 40].indexOf( e.keyCode ) !== -1){
+                switch(e.keyCode){
+                        case 37: this.handleLeftKeyPress();
+                            break;
+                        case 39: this.handleRightKeyPress();
+                            break;
+                        case 38: this.handleTopKeyPress();
+                            break;
+                        case 40: this.handleBottomKeyPress();
+                            break;
+                        default:
+                            break;
+                    }
                 }
             }
-        }
-
+    }
     
-
-    //1. reduce -> reduced     -> generate a 2 in remaining area, update score 
-    //          -> not reduced -> grid have spaces do nothing
-    //                         -> grid don't have spaces game over
-    //   
-
-    //2. generate new number
     handleLeftKeyPress = () => {
         let grid = this.getStateCopy();
         grid = this.reduceGridLeft(grid);
@@ -59,17 +55,22 @@ class GameGrid extends Component{
         this.addNewNumberToGrid();
     }
     handleTopKeyPress = () => {
-        this.reduceGridTop();
+        let grid = this.getStateCopy();
+        grid = this.reduceGridTop(grid);
+        this.updateState(grid, 'game');
         this.addNewNumberToGrid();
     }
     handleBottomKeyPress = () => {
-        this.reduceGridBottom();
+        let grid = this.getStateCopy();
+        grid = this.reduceGridBottom(grid);
+        this.updateState(grid, 'game');
         this.addNewNumberToGrid();
     }
 
+
     rotateGrid = (grid, dir) =>{
         switch(dir){
-            case "180":     
+            case "180": {     
                     return _.map(grid, row => {
                         let rot = [];
                         for(let i=0, j=row.length-1; i<j; i++, j--){
@@ -78,6 +79,29 @@ class GameGrid extends Component{
                         }
                         return rot;
                     })
+                }
+            case "90": {
+                    let rot = [];
+                    for(let i=grid[0].length-1; i>=0; i--){
+                        let rotRow = [];
+                        for(let j=0; j<grid.length; j++){
+                            rotRow.push(grid[j][i]);
+                        }
+                        rot.push(rotRow)
+                    }
+                    return rot;
+                }
+            case "270":{
+                    let rot = [];
+                    for(let i=0; i<grid[0].length; i++){
+                        let rotRow = [];
+                        for(let j=grid.length-1; j>=0; j--){
+                            rotRow.push(grid[j][i]);
+                        }
+                        rot.push(rotRow)
+                    }
+                    return rot;
+                }
             default:
                     return grid;
         }
@@ -134,6 +158,20 @@ class GameGrid extends Component{
         return grid;
     }
 
+    reduceGridTop = (grid) => {
+        grid = this.rotateGrid(grid, "90");
+        this.reduceGridLeft(grid);
+        grid = this.rotateGrid(grid, "270");
+        return grid;
+    }
+
+    reduceGridBottom = (grid) => {
+        grid = this.rotateGrid(grid, "270");
+        this.reduceGridLeft(grid);
+        grid = this.rotateGrid(grid, "90");
+        return grid;
+    }
+
     getRandomFreeCell = () => {
         let emptyCells=0;
         _.forEach(this.state.game, row =>{
@@ -168,8 +206,10 @@ class GameGrid extends Component{
     addNewNumberToGrid = () => {
         console.log("called");
         let {rowNum, colNum} = this.getRandomFreeCell();
-        if( rowNum === -1)
-            throw Error('grid_full');
+        if( rowNum === -1){
+            this.updateState('over', 'game_status')
+            return;
+        }
         const grid = this.getStateCopy();
         grid[rowNum][colNum] = (Math.random()*2 <= 1) ? 2 : 4;
         this.updateState(grid, 'game');
@@ -183,11 +223,25 @@ class GameGrid extends Component{
     }
 
     getStateCopy = () => {
-        return _.map(this.state.game, (row, i) => {
-            return _.map(row, (col, j) => {
-                return col;
-            });
-        });
+        let rot = [];
+        for(let i=0; i<this.state.game.length; i++){
+            let rotRow = [];
+            for(let j=0; j<this.state.game[0].length; j++){
+                rotRow.push(this.state.game[i][j]);
+            }
+            rot.push(rotRow)
+        }
+        return rot;
+    }
+
+    startGameAction = () => {
+        this.updateState('started', 'game_status')
+    }
+
+    getClassName = (num) => {
+        if(num===0)
+            return "";
+        return "num"+num;
     }
 
     render(){
@@ -196,10 +250,11 @@ class GameGrid extends Component{
             <div className = "grid_container">
             {
                 this.state.game.map((row,i) => {
-                 return row.map((col,j) => (<div className="grid_cell" key={i*4+j}>{col}</div>))}
+                 return row.map((col,j) => (<div className={"grid_cell "+ this.getClassName(col)} key={i*4+j}>{col === 0? "": col}</div>))}
                 )
             }
             </div>
+            <button className="start" onClick={this.startGameAction}>Start Game</button>
         </div>
         )
     }
